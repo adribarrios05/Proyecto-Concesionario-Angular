9eb0937d53fdb198361ceb84db6bfe3ed2b4b0fd
Adrián <abarmar0501@g.educaand.es>
2025-03-07 12:14:22 +0100

    Suscripciones customer

--- src/app/folder/pages/customers/customers.page.ts
+++ src/app/folder/pages/customers/customers.page.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { InfiniteScrollCustomEvent, LoadingController, ModalController, Platform, ToastController } from '@ionic/angular';
 import { TranslateService } from '@ngx-translate/core';
 import { BehaviorSubject, lastValueFrom, Observable } from 'rxjs';
 import { CustomerModalComponent } from 'src/app/components/customer-modal/customer-modal.component';
 import { User } from 'src/app/core/models/auth.model';
+import { Car } from 'src/app/core/models/car.model';
 import { Customer } from 'src/app/core/models/customer.model';
 import { Paginated } from 'src/app/core/models/paginated.model';
+import { CUSTOMER_COLLECTION_SUBSCRIPTION_TOKEN } from 'src/app/core/repositories/repository.tokens';
 import { BaseAuthenticationService } from 'src/app/core/services/impl/base-authentication.service';
 import { BaseMediaService } from 'src/app/core/services/impl/base-media.service';
 import { CustomerService } from 'src/app/core/services/impl/customer.service';
+import { ICollectionSubscription, CollectionChange } from 'src/app/core/services/interfaces/collection-subscription.interface';
 
 @Component({
   selector: 'app-customers',
@@ -22,16 +25,47 @@ export class CustomersPage implements OnInit {
   _users:BehaviorSubject<User[]> = new BehaviorSubject<User[]>([]);
   users$:Observable<User[]> = this._users.asObservable();
   isWeb: boolean = false
+  private loadedIds: Set<string> = new Set(); 
+
   constructor(
     private customerService: CustomerService,
     private modalCtrl: ModalController,
-    private platform: Platform
+    private platform: Platform,
+    @Inject(CUSTOMER_COLLECTION_SUBSCRIPTION_TOKEN) private customerSubscription: ICollectionSubscription<Customer>
+    
   ) {
     this.isWeb = this.platform.is('desktop')
   }
 
   ngOnInit() {
     this.loadCustomers()
+    this.customerSubscription.subscribe('customers').subscribe((change: CollectionChange<Customer>) => {
+          const currentCustomers = [...this._customers.value]
+    
+          // Solo procesar cambios de documentos que ya tenemos cargados
+          if (!this.loadedIds.has(change.id) && change.type !== 'added') {
+            return;
+          }
+    
+          switch(change.type) {
+            case 'added':
+            case 'modified':
+              const index = currentCustomers.findIndex(p => p.id === change.id);
+              if (index >= 0) {
+                currentCustomers[index] = change.data!;
+              }
+              break;
+            case 'removed':
+              const removeIndex = currentCustomers.findIndex(p => p.id === change.id);
+              if (removeIndex >= 0) {
+                currentCustomers.splice(removeIndex, 1);
+                this.loadedIds.delete(change.id);
+              }
+              break;
+          }
+          this._customers.next(currentCustomers);
+    
+        })
   }
 
   page: number = 1
